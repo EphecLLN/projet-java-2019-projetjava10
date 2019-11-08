50a3f51c300e86b2369d016f69591831eb2c2d90
GauthB <gauthier.bohyn@gmail.com>
2019-11-08 10:58:43 +0100

    images icon

--- Space Invaders/src/controller/Img.java
+++ Space Invaders/src/controller/Img.java
@@ -3,10 +3,23 @@
  */
 package controller;
 
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
 /**
  * @author gauthierbohyn
  *
  */
 public class Img {
+	
+	//ImageIcon iconInvader, iconShip;
 
+	ImageIcon iInvader = new ImageIcon("invader.gif");
+	Image iconInvader = iInvader.getImage();
+	
+	ImageIcon iEnemy = new ImageIcon("gif");
+	Image iconEnemy = iEnemy.getImage();
+	
+	
 }

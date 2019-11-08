2393d58ee33b23b3e5626da46c39a48d32825f75
GauthB <gauthier.bohyn@gmail.com>
2019-11-08 10:37:50 +0100

    update Entity

--- Space Invaders/src/controller/Entity.java
+++ Space Invaders/src/controller/Entity.java
@@ -7,8 +7,62 @@ package controller;
  * @author gauthierbohyn
  *
  */
-public class Entity {
+public abstract class Entity {
+
+	private int x;
+	private int y;
+	
+	public int height;
+	public int width;
+	
+	public boolean isAlive = true;
+	
+	public boolean isAlive() {
+		return isAlive;
+	}
+
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+
+
+
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
 
 	
-	//test
+
+	/**
+	 * @return the height
+	 */
+	public int getHeight() {
+		return height;
+	}
+
+
+
+	/**
+	 * @return the width
+	 */
+	public int getWidth() {
+		return width;
+	}
+
+
+	/**
+	 * @param isAlive the isAlive to set
+	 */
+	public void setAlive(boolean isAlive) {
+		this.isAlive = isAlive;
+	}
+	
+	public abstract void nextTurn();
+	
 }

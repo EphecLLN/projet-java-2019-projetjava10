a5f23a59d49d0271158f7d2da991105f1febc0a2
Bohyn Gauthier <39745082+GauthB@users.noreply.github.com>
2019-11-15 11:28:00 +0100

    Update Map.java

--- Space Invaders/src/view/Map.java
+++ Space Invaders/src/view/Map.java
@@ -13,10 +13,10 @@ public class Map {
 	public final static int MAP_WIDTH=1000;
 	
 	
-	public final static int SHIP_HEIGHT=33;
-	public final static int SHIP_WIDTH=80;
-	public final static int SHIP_START_X=450;
-	public final static int SHIP_START_Y=400;
+	public final static int VAISSEAU_HEIGHT=33;
+	public final static int VAISSEAUP_WIDTH=80;
+	public final static int VAISSEAU_START_X=450;
+	public final static int VAISSEAU_START_Y=400;
 	
 	public final static int MOVE_VAISSEAU=10;
 

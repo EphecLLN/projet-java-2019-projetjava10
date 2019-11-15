a4672934e31aa648a6288625ff2f815091a76f4e
arnogodart95 <44059862+arnogodart95@users.noreply.github.com>
2019-11-15 11:30:44 +0100

    Update jeu.java

--- Space Invaders/src/model/jeu.java
+++ Space Invaders/src/model/jeu.java
@@ -26,6 +26,22 @@ public class Jeu {
 		vaisseau = new Vaisseau(Map.VAISSEAU_START_X, Map.VAISSEAU_START_Y);
 		
 	}
+
+	/**
+	 * @return the vaisseau
+	 */
+	public Vaisseau getVaisseau() {
+		return vaisseau;
+	}
+
+	/**
+	 * @param vaisseau the vaisseau to set
+	 */
+	public void setVaisseau(Vaisseau vaisseau) {
+		this.vaisseau = vaisseau;
+	}
+	
+	
 	
 
 }

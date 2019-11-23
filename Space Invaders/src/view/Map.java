558c53b55a39423c4f8df2d60afa24d00cbe6970
Bohyn Gauthier <39745082+GauthB@users.noreply.github.com>
2019-11-23 14:30:45 +0100

    update fonctionnel

--- Space Invaders/src/view/Map.java
+++ Space Invaders/src/view/Map.java
@@ -12,23 +12,23 @@ public class Map {
 	/**
 	 * Taille de la carte (hauteur et largeur)
 	 */
-	public final static int MAP_HEIGHT=400;
-	public final static int MAP_WIDTH=2000;
+	public final static int MAP_HEIGHT=500;
+	public final static int MAP_WIDTH=1000;
 	
 	/**
 	 * Taille du vaisseau
 	 */
-	public final static int VAISSEAU_HEIGHT=40;
-	public final static int VAISSEAU_WIDTH=100;
+	public final static int VAISSEAU_HEIGHT=33;
+	public final static int VAISSEAU_WIDTH=80;
 	
 	/**
 	 * Position dans laquelle le vaisseau commence
 	 */
-	public final static int VAISSEAU_START_X=500;
-	public final static int VAISSEAU_START_Y=450;
+	public final static int VAISSEAU_START_X=450;
+	public final static int VAISSEAU_START_Y=400;
 	/**
 	 * Bouge de 15 vers la droite ou vers la gauche
 	 */
-	public final static int MOVE_VAISSEAU=15;
+	public final static int MOVE_VAISSEAU=10;
 
 }

4e0395ef90e83a4c1d0bd096308e9adf7acc7b77
Cyrton <55984145+Cyrton@users.noreply.github.com>
2019-11-27 11:55:56 +0100

    Update Jeu.java

--- Space Invaders/src/model/Jeu.java
+++ Space Invaders/src/model/Jeu.java
@@ -6,8 +6,6 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-import view.Map;
-
 /**
  * @author gauthierbohyn
  * Classe representant le jeu
@@ -17,19 +15,30 @@ public class Jeu {
 	/**
 	 * On cree le Vaisseau
 	 */
-	private List<Ennemi> ennemi;
-
+	private List<Ennemi> ennemis;
+	private List<Missile> missiles;
 	protected Vaisseau vaisseau;
+	private Random rand;
 	
 	public Jeu() {
-		ennemi = new ArrayList<Ennemi>();
+		ennemis = new ArrayList<Ennemi>();
+		missiles = new ArrayList<Missile>();
+		rand = new Random();
 	}
 	
 	void createEnnemi(int x, int y, boolean dir) {
-		ennemi.add(new Ennemi(x, y, dir));
+		ennemis.add(new Ennemi(x, y, dir));
 	}
 	
+	Missile createMissile(int x, int y, boolean joueur) {
+		Missile m = new Missile(x, y, joueur);
+		missiles.add(m);
+		return m;
+	}
 	
+	public void nextTurn() {
+		
+	}
 	
 	public void initialiser() {
 		
@@ -41,11 +50,60 @@ public class Jeu {
 		createEnnemi(-300,Map.ENNEMI_START_Y,true);
 	}
 	
-	public void moveInvaders() {
-		for (Ennemi i : ennemi) i.nextTurn();
+	private boolean checkCrash(Entity a , Entity b) {
+		if ((b.getX() > a.getX()+a.getLargeur()) || (b.getX()+b.getLargeur() < a.getX())) {
+			return false;
+		} else {
+			if ((b.getY()>a.getY()+a.getHauteur()) || (b.getY()+b.getHauteur() < a.getY())) {
+				return false;
+			} else {
+				System.out.println("crash : "+a.getClass().getName()+" "+b.getClass().getName());
+				System.out.println("A("+a.getX()+","+a.getY()+"), B("+b.getX()+","+b.getY()+")");
+				return true;
+			}
+		}
 	}
 	
-
+	public boolean checkJoueurCrash() {
+		for (Missile m : missiles) {
+			if (!m.joueur && checkCrash(vaisseau, m)) return true;
+		}
+		for (Ennemi i : ennemis) {
+			if (checkCrash(vaisseau, i)) return true;
+		}
+		return false;
+	}
+	
+	
+	public List<Ennemi> checkEnnemiCrash() {
+		List<Ennemi> crashed = new ArrayList<Ennemi>();
+		for (Missile m : missiles) {
+			if (m.player) {
+				for (Ennemi i : ennemis) {
+					if (checkCrash(i, m)) crashed.add(i);
+				}
+			}
+		}
+		return crashed;
+	}
+	
+	public void moveEnnemis() {
+		for (Ennemi i : ennemis) i.nextTurn();
+	}
+	
+	public void moveMissiles() {
+		for (Missile m : missiles) m.nextTurn();
+	}
+	
+	public void tirEnnemis() {
+		for (Ennemi i : ennemis) {
+			if (i.isEnVie()) {
+			int dice = rand.nextInt(100) ;
+			if (dice <5) createMissile(i.getX()+(i.getLargeur()/2), i.getY(), false);
+			}
+		}
+	}
+	
 	/**
 	 * @return the vaisseau
 	 */
@@ -61,17 +119,17 @@ public class Jeu {
 	}
 
 	/**
-	 * @return the ennemi
+	 * @return the ennemis
 	 */
 	public List<Ennemi> getEnnemi() {
-		return ennemi;
+		return ennemis;
 	}
 
 	/**
 	 * @param ennemi the ennemi to set
 	 */
 	public void setEnnemi(List<Ennemi> ennemi) {
-		this.ennemi = ennemi;
+		this.ennemis = ennemi;
 	}
 	
 	

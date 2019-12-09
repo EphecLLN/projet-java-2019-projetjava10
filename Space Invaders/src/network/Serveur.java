fcb61e7783e34c00c3e99c244c8f8ad2d2f6d6a2
arnogodart95 <44059862+arnogodart95@users.noreply.github.com>
2019-12-09 15:20:17 +0100

    Update Serveur.java

--- Space Invaders/src/network/Serveur.java
+++ Space Invaders/src/network/Serveur.java
@@ -20,22 +20,19 @@ public class Serveur {
 	protected DataInputStream dataIn;
 	protected DataOutputStream dataOut;
 	
-	
-	
-	
-	public Serveur (int port) {
-		this.port=port;
-	}
+
 	
 	// Etablir une connexion
 	public void connect() throws Exception {
 		try {
-			ss = new ServerSocket(port);
+			ss = new ServerSocket(0); // 0 signifie que l'on prend le premier port disponible
+			System.out.println(ss.getLocalPort()); // Afficher le port disponible en console
 			System.out.println("En attente du client");
+			ss.setSoTimeout(10000); // Timer permettant de fermer le socket en cas de non-connexion (après 10 secondes ici)
 			s = ss.accept();
 			System.out.println("Connecté");
-			dataIn = new DataInputStream(s.getInputStream());
-			dataOut = new DataOutputStream(s.getOutputStream());
+			dataIn = new DataInputStream(s.getInputStream()); // Données entrantes
+			dataOut = new DataOutputStream(s.getOutputStream()); // Données sortantes
 			
 			
 		} catch (Exception e) {

f4d248273f9951e732fca49db5b13eaef11d26c2
arnogodart95 <44059862+arnogodart95@users.noreply.github.com>
2019-12-09 14:45:02 +0100

    Update Client.java

--- Space Invaders/src/network/Client.java
+++ Space Invaders/src/network/Client.java
@@ -16,8 +16,32 @@ public class Client {
 	protected DataInputStream dataIn;
 	protected DataOutputStream dataOut;
 	protected int port;
+	protected String ip = "";
 	
 	
+	// Etablir la connexion
+	public void connect() throws Exception {
+		try {
+			s = new Socket(ip, port);
+			System.out.println("Socket créé");
+			dataIn = new DataInputStream(s.getInputStream());
+			dataOut = new DataOutputStream(s.getOutputStream());
+			System.out.println("Connecté");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Fermer la connexion
+	public void disconnect() throws Exception{
+		try {
+			s.close();
+			System.out.println("Déconnecté");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	
 	
